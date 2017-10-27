b73e001528d1a7791aeb9a8375b9a4230cf24f5c
Rezky Firmansyah <rezkyfm1@gmail.com>
2017-10-27 20:17:36 +0700

    initial commit

---
+++ config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  //Theme
+  theme: "lightBlue",
+
+  //Email Account
+  host: "smtp.mailtrap.io", //Change to your email smtp
+  port: "465", //Change to your email port
+  user: "user", //Change to your email username
+  pass: "password", //Change to your email password
+
+  //Email Message
+  from: '"Contact Me" <noreply@example.com>', // Sender address
+  to: 'youremail@.com', // List of receivers
+  subject: 'Contact Us', // Subject
+};
