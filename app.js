a1d3e1317e75a63d53ec0f57774a38f81a580d73
Rezky Firmansyah <rezkyfm1@gmail.com>
2017-10-28 12:19:16 +0700

    fix config

--- app.js
+++ app.js
@@ -30,70 +30,60 @@ app.get ('/', (req, res) => {
 //Post Emaul Request
 app.post('/send', (req, res) => {
 
+  //Email Template
+  const output = `
+    <p>You have a message</p>
+    <h3>Contact Details</h3>
+    <p>Name: ${req.body.name}</p>
+    <p>Email: ${req.body.email}</p>
+    <h3>Message</h3>
+    <p>${req.body.message}</p>
+  `;
+
+  //Alert if success sending email
+  const successAlert = `
+    <div class="uk-alert-success" uk-alert>
+        <a class="uk-alert-close" uk-close></a>
+        <p>Message has been sent</p>
+    </div>
+  `;
+
+  //Alert if fail sending email
+  const failAlert = `
+    <div class="uk-alert-warning" uk-alert>
+        <a class="uk-alert-close" uk-close></a>
+        <p>Failed to send message. Please refresh this page</p>
+    </div>
+  `;
+
   // Generate SMTP service account from ethereal.email
   nodemailer.createTestAccount((err, account) => {
-    if (err) {
-        console.error('Failed to create a testing account. ' + err.message);
-        return process.exit(1);
-    }
-
-    console.log('Credentials obtained, sending message...');
-
-    //Email Template
-    const output = `
-      <p>You have a message</p>
-      <h3>Contact Details</h3>
-      <p>Name: ${req.body.name}</p>
-      <p>Email: ${req.body.email}</p>
-      <h3>Message</h3>
-      <p>${req.body.message}</p>
-    `;
-
-    //Alert if success sending email
-    const successAlert = `
-      <div class="uk-alert-success" uk-alert>
-          <a class="uk-alert-close" uk-close></a>
-          <p>Message has been sent</p>
-      </div>
-    `;
-
-    //Alert if fail sending email
-    const failAlert = `
-      <div class="uk-alert-warning" uk-alert>
-          <a class="uk-alert-close" uk-close></a>
-          <p>Failed to send message. Please refresh this page</p>
-      </div>
-    `;
-
 
     // create reusable transporter object using the default SMTP transport
     let transporter = nodemailer.createTransport({
-        host:  account.smtp.host,
-        port: account.smtp.port,
-        secure: false,
-        auth: {
-            user: account.user,
-            pass: account.pass,
-        },
-        tls:{
-          rejectUnauthorized:false
-        }
-      });
+      host: 'smtp.ethereal.email',
+      port: 587,
+      secure: false,
+      auth: {
+        user: account.user,
+        pass: account.pass
+      }
+    });
 
-      // setup email data with unicode symbols
-      let mailOptions = {
-          from: config.from,
-          to: config.to,
-          subject: config.subject,
-          html: output
-      };
+    // setup email data with unicode symbols
+    let mailOptions = {
+        from: config.from,
+        to: config.to,
+        subject: config.subject,
+        html: output
+    };
 
-      // send mail with defined transport object
-      transporter.sendMail(mailOptions, (error, info) => {
-        if (error) {
-          res.render(config.theme, {msg: failAlert});
-        }
-        res.render(config.theme, {msg: successAlert});
-      });
-    })
+    // send mail with defined transport object
+    transporter.sendMail(mailOptions, (error, info) => {
+      if (error) {
+        res.render(config.theme, {msg: failAlert});
+      }
+      res.render(config.theme, {msg: successAlert});
+    });
   });
+});
