2fb0d363b3662ca1a4f60bb57cccb5796f6dac01
Rezky Firmansyah <rezkyfm1@gmail.com>
2017-10-28 12:59:53 +0700

    remove package

---
+++ configTest.js
@@ -0,0 +1,15 @@
+module.exports = {
+  //Theme
+  theme: "lightBlue",
+
+  //Email Account
+  host: "smtp.mailtrap.io", //Change to your email smtp
+  port: "465", //Change to your email port
+  user: "d8b815627b7164", //Change to your email username
+  pass: "e97a430a65e0c3", //Change to your email password
+
+  //Email Message
+  from: '"Contact Me" <test@example.com>', // Sender address
+  to: 'b2e98dfb23-a196d4@inbox.mailtrap.io', // List of receivers
+  subject: 'Contact Us', // Subject
+};
